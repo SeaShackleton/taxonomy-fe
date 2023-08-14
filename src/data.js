9020cc6ce41f8b7669b453f69680c677a43b594f
Christian Shackleton <cas7374@gmail.com>
2023-08-14 14:22:48 -0500

    Git init
    Git init

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default {
+  name: "Life",
+  taxons: [
+    {
+      name: "Eucarya"
+	},
+	{
+      name: "Archaea"
+	},   
+	{
+      name: "Bacteria",
+	  taxons: [
+		{ name: "Accounts payable", value: 53010 },
+		{ name: "Accrued salaries", value: 23554 },
+		{ name: "Unearned revenue", value: 253322 },
+		{ name: "Commercial paper", value: 326008 },
+		{ name: "Current portion of long-term debt", value: 112431 },
+		{ name: "Funds held for others", value: 2500 },
+		{ name: "Other current liabilities", value: 71036 }
+	  ]	  
+	}
+  ]
+};
