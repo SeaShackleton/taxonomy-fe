e908262247fc1cb705fc484ebc1604fec841de23
Christian Shackleton <cas7374@gmail.com>
2023-08-14 17:49:39 -0500

    ajax as prop
    Pulled ajax call and implemented it as a prop for the component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import SunburstChart from "./SunburstChart";
 
 function App() {
+	const [data, setData] = useState();
+	const [isLoading, setLoading] = useState(true);
+	
+	const fetchTaxonData = () => {
+		fetch("http://localhost:8000/api/taxonomy/1")
+			.then(response => {
+				return response.json()
+			})
+			.then(d => {
+				setData(d);
+				setLoading(false);
+			})
+	};	
+		
+	useEffect(() => {
+		fetchTaxonData();
+	  }, []);
+	
+	if(isLoading){
+		return <div>Loading</div>
+	}	
 	return (
 		<div className="App">
-			<SunburstChart />
+			<SunburstChart data={data}/>
 		</div>
 	);
 }
--- src/SunburstChart.js
+++ src/SunburstChart.js
@@ -1,38 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Sunburst from 'sunburst-chart'
 import * as d3 from 'd3'
-import data from "./data";
 
 
-export default function SunburstChart() {
+export default function SunburstChart(props) {
 	const chart = React.useRef(null);
 	const ref = React.useRef(null)
 	const color = d3.scaleOrdinal(d3.schemePaired);
-	const [data2, setData2] = useState([]);
-
-
-	const fetchTaxonData = () => {
-		fetch("http://localhost:8000/api/taxonomy/1")
-			.then(response => {
-				return response.json()
-				//return response;
-			})
-			.then(data2 => {
-				//var t = JSON.parse(data2);
-				console.log( data2 );
-				setData2(data2);
-			})
-	};
-
-	useEffect(() => {
-		fetchTaxonData();
-	  }, []);
-	  
 	  
 	React.useEffect(() => {
 		chart.current = Sunburst()
 		chart.current
-		  .data(data)
+		  .data(props.data)
 		  .width(500)
 		  .height(500)
 		  .color((d, parent) => color(parent ? parent.data.name : null))
